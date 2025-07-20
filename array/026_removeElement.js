724537232d22e11be8d2fba0391b7438d0ddd2e6
Sarfaraj028 <sarfaraja0869@gmail.com>
2025-07-20 20:17:52 +0530

    repo clean-up + adding leetcode 026 problem

---
+++ array/026_removeElement.js
@@ -0,0 +1,19 @@
+/**
+ * 🔥 LeetCode 26 - Remove Element
+ * Given an integer array nums and an integer val, remove all occurrences of val in-place.
+ * Return the number of elements not equal to val.
+ * 
+ * ✅ Time: O(n)
+ * ✅ Space: O(1)
+ */
+
+function removeElement(nums, val) {
+    let k = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+}
